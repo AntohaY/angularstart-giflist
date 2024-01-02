967c8e7cfcc7f6b8d9753e556e0e1a286a1453f4
antonyakovenko <anton.yakovenko@one-inside.com>
2024-01-02 18:03:42 +0200

    feat: loading gifs, pagination, new components for gif display

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { GifListComponent } from './ui/gif-list.component';
+import { RedditService } from '../shared/data-access/reddit.service';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  template: ` <p>Hello world</p> `,
+  template: `
+    <app-gif-list
+        infiniteScroll
+        (scrolled)="redditService.pagination$.next(redditService.lastKnownGif())"
+        [gifs]="redditService.gifs()"
+        class="grid-container"
+    />
+  `,
+  imports: [GifListComponent, InfiniteScrollModule]
 })
-export default class HomeComponent {}
+export default class HomeComponent {
+    redditService = inject(RedditService);
+}
